bc04561ac98e2ea0354b089d4b7043b2104b03ea
Gurudeva0 <105483083+Gurudeva0@users.noreply.github.com>
2022-11-18 13:04:01 +0530

    Add files via upload

---
+++ LOOP/script.js
@@ -0,0 +1,20 @@
+var person={
+    name:"Guru",
+    gender:"male",
+   
+}
+let personjson=JSON.stringify(person);
+console.log(personjson);
+
+for (var iterator of personjson) {                                               //for of loop
+    console.log(iterator);
+    
+}
+for (let key in person) {                                                         //for in loop
+    console.log(`key is ${key} value is ${person[key]}`);
+   
+}                                                                                 //for loop
+for(let i=0;i<personjson.length;i++){
+    console.log(personjson[i]);
+    
+}
